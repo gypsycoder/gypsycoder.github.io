aee5d6385519628bcb64724fd0b47ddbb91ebd96
Alen Thomas <alenthomas9@gmail.com>
2017-03-17 16:38:30 +0530

    initial commit

---
+++ sw.js
@@ -0,0 +1,18 @@
+/** An empty service worker! */
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open('the-magic-cache').then(function(cache){
+            return cache.addAll([
+                '/game.html',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
